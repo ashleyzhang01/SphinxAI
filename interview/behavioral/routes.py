b3b1f4dc3c574d5075c3e3e102d4dd7296590239
ashleyzhang01 <ashleyzhang10@gmail.com>
2024-04-06 20:26:12 -0400

    generate behavioral

--- interview/behavioral/routes.py
+++ interview/behavioral/routes.py
@@ -2,30 +2,45 @@ from openai import OpenAI
 import os
 from flask import Blueprint, flash, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
+from sqlalchemy.sql.expression import func
+from interview.models import db, Question
 
 from interview.behavioral import ai, process_transcript_section
 
 behavioral = Blueprint('behavioral', __name__)
 
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = './static/uploads'
 
-# A <form> tag is marked with enctype=multipart/form-data and an <input type=file> is placed in that form.
+
+# (multipart form) A <form> tag is marked with enctype=multipart/form-data and an <input type=file> is placed in that form.
 # The application accesses the file from the files dictionary on the request object.
 # use the save() method of the file to save the file permanently somewhere on the filesystem.
-@behavioral.route('/resume-upload', methods=['GET', 'POST'])
-def resume_upload():
+# Should have the resume along with other information for the 
+@behavioral.route('/behavioral', methods=['GET', 'POST'])
+def generate_behavioral():
     if request.method == 'POST':
+        # persona / introduction
+        behavioral_qs = db.session.query(Question).filter(Question.category == 'BEHAVIORAL').order_by(func.random()).limit(3).all()
+        resume_qs = ['Walk me through your resume and tell me about your experiences.']
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file:
             filename = secure_filename(file.filename)
-            file.save(f'static/{filename}')
-            resume_questions = ai.resume_grill(f'static/{filename}')
+            file.save(f'static/resumes/{filename}')
+            resume_qs = ai.resume_grill(f'./static/resumes/{filename}')
+        
+        questions_list = {
+            # 'intro': ...,
+            'behavioral': [question.question for question in behavioral_qs],
+            'resume': resume_qs,
+            'questions': ['Do you have any questions for me?']
+        }
+        return jsonify(questions_list)
+
 
 
 @behavioral.route('/behavioral-feedback', methods=['GET', 'POST'])
