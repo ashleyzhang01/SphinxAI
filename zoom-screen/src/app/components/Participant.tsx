159914d3be9b459ccb4f3218f66c88a6de819794
Kevin Lu <kevinlu625@gmail.com>
2024-04-11 11:25:35 -0400

    bare interview video/mic functionality

---
+++ zoom-screen/src/app/components/Participant.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useEffect, useRef, FC } from 'react';
+import styles from '../styles/Home.module.css'
+
+interface ParticipantProps {
+  name: string;
+}
+
+const Participant: FC<ParticipantProps> = ({ name }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then(stream => {
+        let video = videoRef.current;
+        if (video) {
+          video.srcObject = stream;
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className={styles.card}>
+    <h2>{name} &rarr;</h2>
+    <video ref={videoRef} autoPlay playsInline />
+  </div>
+  );
+}
+
+export default Participant;
