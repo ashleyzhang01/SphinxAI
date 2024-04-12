1d12791e46bca8c6fdda648d9fdc583069c66390
ashleyzhang01 <ashleyzhang10@gmail.com>
2024-04-12 04:12:35 -0400

    interviews page

--- interview-frontend/src/app/home/page.tsx
+++ interview-frontend/src/app/home/page.tsx
@@ -27,17 +27,17 @@ const HomeScreen = (props: any) => {
         <h1 className="text-6xl text-center mt-24">
           SphinxAI
         </h1>
-        <h2 className="text-3xl text-center mt-4">Decode Success, Revolutionize Your Hiring Game with Sphinx AI</h2>
+        <h2 className="text-3xl text-center mt-4">Decode Success, Revolutionize Your Hiring Game</h2>
         <div className="flex items-center mt-16 space-x-8 ml-16 mr-16">
-          <div className="bg-gray-200 shadow-lg p-6 rounded-lg w-1/3">
+          <div className="bg-black bg-opacity-10 shadow-lg p-6 rounded-lg w-1/3">
             <h3 className="text-2xl font-semibold mb-4">Comprehensive Interview Simulation</h3>
             <p>We simulate interview processes for a variety of roles, including software engineering, quantitative trading, consulting, and investment banking. We offer both behavioral and technical interviews, which include coding interviews for SWE, case questions for consulting, etc. </p>
           </div>
-          <div className="bg-gray-200 shadow-lg p-6 rounded-lg w-1/3">
+          <div className="bg-black bg-opacity-10 shadow-lg p-6 rounded-lg w-1/3">
             <h3 className="text-2xl font-semibold mb-4">Full Immersive Interview Experience</h3>
             <p>We provide a full immersive interview experience, with text-to-speech and speech-to-text capabilities, along with a customizable mock interviewer, which uses AI to generate follow up responses/questions. It looks amd feels like you are conversing with a real interviewer.</p>
           </div>
-          <div className="bg-gray-200 shadow-lg p-6 rounded-lg w-1/3">
+          <div className="bg-black bg-opacity-10 shadow-lg p-6 rounded-lg w-1/3">
             <h3 className="text-2xl font-semibold mb-4">Personalized AI Analysis & Training</h3>
             <p>We use AI to provide personalized feedback and analysis on your responses' correctness and clarity, helping you optimize your performance. You'll also get feedback on how to make your points more clear and confident with analysis of eye-tracking, pauses, and filler words. </p>
           </div>
--- interview-frontend/src/app/interviews/page.tsx
+++ interview-frontend/src/app/interviews/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import {useRouter} from "next/navigation"
 import axios from 'axios';
+import NavBar from '@/components/NavBar';
 
 const InterviewsPage = () => {
   const [category, setCategory] = useState('');
@@ -25,7 +26,13 @@ const InterviewsPage = () => {
       if (resume) {
         formData.append('resume', resume);
       }
-      const questions : any = await axios.post('http://localhost:5000/api/behavioral', formData);
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+        },
+      };
+      const questions : any = await axios.post('http://localhost:5000/api/behavioral', formData, config);
       localStorage.setItem("category", category)
       localStorage.setItem("questions", questions)
       router.push('/');
@@ -36,19 +43,35 @@ const InterviewsPage = () => {
 
   return (
     <div>
-      <h1>Interviews</h1>
-      <div>
-        <select value={category} onChange={handleCategoryChange}>
+      <div className="h-screen">
+        <NavBar userData={{ username: "asdf" }} />
+    <div className="container text-center items-center mx-auto px-4 py-8">
+      <h1 className="text-5xl font-bold mt-4 mb-16">Start Interview</h1>
+      <div className="flex items-center mb-4">
+        <select
+          className="border border-gray-300 rounded-lg px-3 py-2 mr-4 ml-64"
+          value={category}
+          onChange={handleCategoryChange}
+        >
           <option value="">Select Category</option>
           <option value="Investment Banking">Investment Banking</option>
           <option value="Consulting">Consulting</option>
           <option value="Quant">Quant</option>
           <option value="Software Engineering">Software Engineering</option>
         </select>
-        <input type="file" onChange={handleResumeChange} />
-        <button onClick={handleNext}>Next</button>
+        <input
+          type="file"
+          onChange={handleResumeChange}
+          className="px-3 py-2 mr-0"
+        />
+        <button
+          onClick={handleNext}
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md"
+        >
+          Next
+        </button>
       </div>
-    </div>
+    </div></div></div>
   );
 };
 
